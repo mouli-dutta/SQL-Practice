faca15bd61b139ee192b1a90f268b112b614859f
Mouli Dutta <117016206+mouli-dutta@users.noreply.github.com>
2025-02-24 14:13:10 +0530

    Update query2.sql

--- query2.sql
+++ query2.sql
@@ -1,3 +1,26 @@
+/*
+Query the list of CITY names starting with vowels (a, e, i, o, u) from STATION. Your result cannot contain duplicates.
+*/
+
 SELECT DISTINCT CITY 
 FROM STATION 
 WHERE CITY REGEXP '^[AEIOU]';
+
+
+/*
+Query the list of CITY names ending with vowels (a, e, i, o, u) from STATION. Your result cannot contain duplicates.
+*/
+
+SELECT DISTINCT CITY 
+FROM STATION 
+WHERE CITY REGEXP '[AEIOU]$';
+
+
+
+/*
+Query the list of CITY names from STATION which have vowels (i.e., a, e, i, o, and u) as both their first and last characters. Your result cannot contain duplicates.
+*/
+
+SELECT DISTINCT CITY 
+FROM STATION 
+WHERE CITY REGEXP '^[AEIOU].*[AEIOU]$';
