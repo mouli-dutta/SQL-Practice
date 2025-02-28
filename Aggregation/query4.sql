364a201699165210eb92a75fd4c90fbee8b60c79
Mouli Dutta <117016206+mouli-dutta@users.noreply.github.com>
2025-02-28 15:55:09 +0530

    Update query4.sql

--- Aggregation/query4.sql
+++ Aggregation/query4.sql
@@ -1,14 +1,49 @@
 /*
 https://www.hackerrank.com/challenges/weather-observation-station-13/problem?isFullScreen=true
 */
-select 
-    round(sum(LAT_N), 4) 
-from station 
-where LAT_N between 38.7880 and 137.2345;
-
+select round(sum(LAT_N), 4) from station where LAT_N between 38.7880 and 137.2345;
 
 
 /*
 https://www.hackerrank.com/challenges/weather-observation-station-14/problem?isFullScreen=true
 */
 select max(round(LAT_N, 4)) from station where LAT_N < 137.2345;
+
+
+/*
+https://www.hackerrank.com/challenges/weather-observation-station-15/problem?isFullScreen=true
+*/
+select round(LONG_W, 4) from station where LAT_N < 137.2345 order by LAT_N desc limit 1;
+
+
+/*
+https://www.hackerrank.com/challenges/weather-observation-station-16/problem?isFullScreen=true
+*/
+select round(LAT_N, 4) from station where LAT_N > 38.7780 order by LAT_N limit 1;
+
+
+/*
+https://www.hackerrank.com/challenges/weather-observation-station-17/problem?isFullScreen=true
+*/
+select round(LONG_W, 4) from station where LAT_N > 38.7780 order by LAT_N limit 1;
+
+
+/*
+https://www.hackerrank.com/challenges/weather-observation-station-18/problem?isFullScreen=true
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
