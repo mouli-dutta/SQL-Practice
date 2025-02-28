5ed87ad5a39d656f34efd9735c622b5385a7d331
Mouli Dutta <117016206+mouli-dutta@users.noreply.github.com>
2025-02-28 15:41:14 +0530

    Update query4.sql

--- Aggregation/query4.sql
+++ Aggregation/query4.sql
@@ -1,8 +1,14 @@
 /*
 https://www.hackerrank.com/challenges/weather-observation-station-13/problem?isFullScreen=true
 */
-
 select 
     round(sum(LAT_N), 4) 
 from station 
 where LAT_N between 38.7880 and 137.2345;
+
+
+
+/*
+https://www.hackerrank.com/challenges/weather-observation-station-14/problem?isFullScreen=true
+*/
+select max(round(LAT_N, 4)) from station where LAT_N < 137.2345;
