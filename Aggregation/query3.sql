25b244612b12a443c72deb487010a1dd90dbfbed
Mouli Dutta <117016206+mouli-dutta@users.noreply.github.com>
2025-02-28 15:24:17 +0530

    Update query3.sql

--- Aggregation/query3.sql
+++ Aggregation/query3.sql
@@ -1,5 +1,11 @@
 /*
-https://www.hackerrank.com/challenges/revising-aggregations-the-average-function/problem?isFullScreen=true
-*/
+https://www.hackerrank.com/challenges/earnings-of-employees/problem?isFullScreen=true*/
 
-select avg(population) from city where district = 'California';
+WITH earnings AS (
+    SELECT employee_id, months * salary AS total_earnings
+    FROM employee
+)
+
+SELECT MAX(total_earnings) AS max_total_earnings, COUNT(*) AS employee_count
+FROM earnings
+WHERE total_earnings = (SELECT MAX(total_earnings) FROM earnings);
