f006a166512f13f4d8c9c73c6cc3c1c35c48f0d1
Mouli Dutta <117016206+mouli-dutta@users.noreply.github.com>
2025-02-28 13:51:16 +0530

    Update query1.sql

--- Aggregation/query1.sql
+++ Aggregation/query1.sql
@@ -1,6 +1,31 @@
 /*
 https://www.hackerrank.com/challenges/revising-aggregations-the-count-function/problem?isFullScreen=true
 */
-
 select count(*) from city where population > 100000;
+
+
+/*
+https://www.hackerrank.com/challenges/revising-aggregations-sum/problem?isFullScreen=true
+*/
+select sum(population) from city where district = 'California';
+
+
+/*
+https://www.hackerrank.com/challenges/revising-aggregations-the-average-function/problem?isFullScreen=true
+*/
+select avg(population) from city where district = 'California';
+
+
+/*
+https://www.hackerrank.com/challenges/japan-population/problem?isFullScreen=true
+*/
+select sum(population) from city where countrycode="JPN";
+
+
+/*
+https://www.hackerrank.com/challenges/population-density-difference/problem?isFullScreen=true
+*/
+select max(population) - min(population) from city;
+
+
 
